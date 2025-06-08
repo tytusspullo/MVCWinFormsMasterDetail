586b3689db400a11ee2393d3bf02ff6bfe3465e3
Grzegorz Piszczek <grzegorzpiszczek@gmail.com>
2025-06-08 19:52:35 +0200

    aa

---
+++ MVCWinFormsMasterDetailTests/GrupaPracowniczaTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using MVCWinFormsMasterDetail;
+using System.Linq;
+
+namespace MVCWinFormsMasterDetailTests
+{
+    [TestClass]
+    public class GrupaPracowniczaTests
+    {
+
+        [TestMethod]
+        public void DodajGrupaPracownicza()
+        {
+            FrmGrupyPracownicze view = new FrmGrupyPracownicze();
+            var grupyPracownicze = new List<GrupaPracownicza>();
+            var controller = new GrupyPracowniczeController(view, grupyPracownicze);
+            controller.AddGrupaPracownicza();
+            controller.SaveGrupaPracownicza();
+            
+            Assert.IsTrue((grupyPracownicze.Count == 1));
+        }
+
+        [TestMethod]
+        public void EdytujGrupaPracownicza()
+        {
+            string newName = "Nowak-Kowalski Adam";
+            string changedId = "2";
+
+            FrmGrupyPracownicze view = new FrmGrupyPracownicze();
+            var grupyPracownicze = new List<GrupaPracownicza>();
+            grupyPracownicze.Add(new GrupaPracownicza(1, "Nowak Piotr"));
+            grupyPracownicze.Add(new GrupaPracownicza(2, "Nowak Adam"));
+            var controller = new GrupyPracowniczeController(view, grupyPracownicze);
+            
+            controller.SelectedGrupaPracowniczaChanged("2");
+            view.GrupaPracowniczaNazwa = newName;
+            controller.EditGrupaPracownicza();
+            controller.SaveGrupaPracownicza();
+
+            var grupaPracownicza = grupyPracownicze.SingleOrDefault(w => w.IdGrupyPracowniczej.ToString() == changedId);
+
+            Assert.IsTrue(grupaPracownicza.NazwaGrupyPracowniczej == newName);
+        }
+
+        [TestMethod]
+        public void UsunGrupaPracownicza()
+        {
+            FrmGrupyPracownicze view = new FrmGrupyPracownicze();
+            var grupyPracownicze = new List<GrupaPracownicza>();
+            grupyPracownicze.Add(new GrupaPracownicza(1, "Nowak Piotr"));
+            grupyPracownicze.Add(new GrupaPracownicza(2, "Nowak Adam"));
+            grupyPracownicze.Add(new GrupaPracownicza(3, "Nowak Ryszard"));
+            var controller = new GrupyPracowniczeController(view, grupyPracownicze);
+
+            controller.SelectedGrupaPracowniczaChanged("2");
+            controller.RemoveGrupaPracownicza();
+
+
+            Assert.IsTrue(grupyPracownicze.Count == 2);
+
+        }
+    }
+}
---
+++ MVCWinFormsMasterDetailTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MVCWinFormsMasterDetailTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVCWinFormsMasterDetailTests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("de0f1646-55a7-4fd7-bef8-bf65e1e6173f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
